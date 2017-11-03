6967a1b871882029678d8ce67cd9d1877dd9b8b6
wizeek <wizeek80@gmail.com>
2017-11-04 01:25:36 +0300

    added header and build components

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from "@angular/router";
+import { BuildComponent } from './build/build.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: 'build',
+                component: BuildComponent
+            }
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        BuildComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
