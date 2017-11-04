dba8bd623415d639a06bdcc708a44736c4ed002d
wizeek <wizeek80@gmail.com>
2017-11-05 00:41:36 +0300

    added about and page-not-found pages

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from '../builds/list/list.component';
-import { SearchComponent } from '../players/search/search.component';
+import { ListComponent } from './builds/list/list.component';
+import { SearchComponent } from './players/search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/builds', pathMatch: 'full' },
     { path: 'builds', component: ListComponent },
     { path: 'players', component: SearchComponent },
-    { path: '**', redirectTo: '/builds', pathMatch: 'full' }
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BuildsModule } from '../builds/builds.module';
-import { PlayersModule } from '../players/players.module';
+import { BuildsModule } from './builds/builds.module';
+import { PlayersModule } from './players/players.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
     imports: [
@@ -15,7 +17,9 @@ import { PlayersModule } from '../players/players.module';
     ],
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        PageNotFoundComponent,
+        AboutComponent
     ],
     bootstrap: [
         AppComponent
