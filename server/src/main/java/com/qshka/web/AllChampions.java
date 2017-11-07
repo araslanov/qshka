3052d9184cb32ecc5f27fe9f9558717d4ac0b51e
wizeek <wizeek80@gmail.com>
2017-11-08 00:12:20 +0300

    added a test call to riot api

---
+++ server/src/main/java/com/qshka/web/AllChampions.java
@@ -0,0 +1,69 @@
+package com.qshka.web;
+
+import java.util.Map;
+
+public class AllChampions {
+    private String type;
+    private String version;
+    private Map<String, Champion> data;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return version;
+    }
+
+    class Champion {
+        private String title;
+        private int id;
+        private String key;
+        private String name;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
--- server/src/main/java/com/qshka/web/ChampionController.java
+++ server/src/main/java/com/qshka/web/ChampionController.java
@@ -1,8 +1,15 @@
 package com.qshka.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +17,27 @@ import java.util.List;
 @CrossOrigin
 @RestController
 public class ChampionController {
+    @Value("${API_KEY}")
+    private String apiKey;
+
+    @GetMapping("/rito")
+    AllChampions getSomething() {
+        final String uri = "https://euw1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=true";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Riot-Token", apiKey);
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+
+        ResponseEntity<AllChampions> response =  restTemplate.exchange(uri, HttpMethod.GET, entity,
+                new ParameterizedTypeReference<AllChampions>(){});
+
+        System.out.println(response.getBody());
+        return response.getBody();
+    }
+
     @GetMapping("/champions")
     List<String> getChampionNames() {
         return Arrays.asList(
